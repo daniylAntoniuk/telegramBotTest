9f7ff67cb6de63dbaecc7987e071a0447f44a8df
Daniil <danik22122005@gmail.com>
2020-11-21 15:37:48 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelegramClient telegramClient = new TelegramClient();
+            telegramClient.StartClient();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TelegramClient.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    class Poll
+    {
+        public string PollId { get; set; }
+        public Chat ChatId { get; set; }
+    }
+    public class TelegramClient
+    {
+        static ITelegramBotClient botClient;
+        List<Poll> Polls = new List<Poll>();
+        public void StartClient()
+        {
+            botClient = new TelegramBotClient("1452921547:AAE31ilXZ7sykFkOHlCC84FDITD6RDaNib0");
+
+            var me = botClient.GetMeAsync().Result;
+            botClient.OnUpdate += BotClient_OnUpdate;
+            botClient.OnMessage += BotClient_OnMessage;
+            botClient.StartReceiving();
+        }
+
+        private async void BotClient_OnUpdate(object sender, Telegram.Bot.Args.UpdateEventArgs e)
+        {
+            if (e.Update.PollAnswer != null)
+            {
+                //await botClient.SendTextMessageAsync(chatId: Polls.FirstOrDefault(x=>x.PollId == e.Update.PollAnswer.PollId).ChatId, text:e.Update.PollAnswer.OptionIds);
+                var chatId = Polls.FirstOrDefault(x => x.PollId == e.Update.PollAnswer.PollId);
+                if (e.Update.PollAnswer.OptionIds.Length != 0 && chatId !=null)
+                {
+                    if (e.Update.PollAnswer.OptionIds[0] == 0)
+                    {
+                        List<string> answers = new List<string>();
+                        answers.Add("Bye");
+                        answers.Add("I`m Bad !");
+                        var id = await botClient.SendPollAsync(chatId: chatId.ChatId, question: "Why are you nice ?", answers, isAnonymous: false);
+                    }
+                    else if (e.Update.PollAnswer.OptionIds[0] == 1)
+                    {
+                        List<string> answers = new List<string>();
+                        answers.Add("Bye");
+                        answers.Add("I`m Nice !");
+                        var id = await botClient.SendPollAsync(chatId: chatId.ChatId, question: "Why are you bad ?", answers, isAnonymous: false);
+                    }
+                }
+            }
+        }
+        private async void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Poll != null ? e.Message.Poll.TotalVoterCount : "");
+
+            if (e.Message.Text != null)
+            {
+                if (e.Message.Text.ToLower() == "hello")
+                {
+                    List<string> answers = new List<string>();
+                    answers.Add("Nice");
+                    answers.Add("Bad");
+                    var id = await botClient.SendPollAsync(chatId: e.Message.Chat, question: "How are you ?", answers, isAnonymous: false);
+                    Polls.Add(new Poll
+                    {
+                        ChatId = e.Message.Chat,
+                        PollId = id.Poll.Id
+                    });
+                    //await botClient.SendStickerAsync(
+                    //    chatId: e.Message.Chat,
+                    //    sticker: "https://fs13.fex.net:443/download/2521380597"
+                    //    );
+                }
+            }
+        }
+    }
+}
